b46b85bca4c712fee97bfe7a968eb321295bf938
Saleh Mahagney <saleh@salehs-mbp.home>
2020-12-14 16:55:50 +0200

    refactor, fetch data before render for ssr

--- server/src/client/actions/index.js
+++ server/src/client/actions/index.js
@@ -2,7 +2,17 @@ import axios from "axios";
 
 export const FETCH_USERS = "fetch_users";
 export const fetchUsers = () => async (dispatch) => {
-  const res = await axios.get("http://react-ssr-api.herokuapp.com/users");
+  let res = "";
+  try {
+    res = await axios.get("http://react-ssr-api.herokuapp.com/users");
+  } catch (err) {
+    res = {
+      data: [
+        { id: 2, name: "John" },
+        { id: 4, name: "Vas" },
+      ],
+    };
+  }
   dispatch({
     type: FETCH_USERS,
     payload: res,
--- server/src/index.js
+++ server/src/index.js
@@ -2,13 +2,25 @@ import "babel-polyfill";
 import express from "express";
 import renderer from "./helpers/renderer";
 import createStore from "./helpers/createStore";
+import Routes from "./client/Routes";
+import matchRoutes from "react-router-config/matchRoutes";
 
 const app = express();
 app.use(express.static("public"));
 app.get("*", (req, res) => {
   const store = createStore();
 
-  res.send(renderer(req, store));
+  const promises = matchRoutes(Routes, req.path).map(({ route }) => {
+    return route.loadData ? route.loadData(store) : null;
+  });
+
+  console.log(promises);
+  Promise.all(promises)
+    .then((result) => {
+      console.log(result);
+      res.send(renderer(req, store));
+    })
+    .catch((err) => console.log("err", err));
 });
 
 app.listen(3000, () => {
